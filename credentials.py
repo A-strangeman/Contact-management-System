e479e4495739051e8943b8fde34ada41a8727c86
A-strangeman <roshankumar.22211261@viit.ac.in>
2024-09-18 21:51:32 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# User Credentials
+host = 'localhost'
+user = 'roshan'
+password = 'WORLDW@r'
+database = 'contact_management'
---
+++ custom.py
@@ -0,0 +1,10 @@
+color_1 = "wheat"
+color_2 = "tan"
+color_3 = "black"
+color_4 = "aliceblue"
+color_5 = "red"
+color_6 = "green3"
+font_1 = "times new roman"
+font_2 = "helvetica"
+
+columns = ('first_name', 'last_name', 'address', 'contact', 'email')
